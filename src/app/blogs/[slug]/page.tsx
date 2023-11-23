e710c5d0e69241d1d1c08183d3d40af5851d1ee4
sushaz9 <susannahelisabeth1@gmail.com>
2023-11-23 11:31:16 +0000

    dynamic bits

--- src/app/blogs/[slug]/page.tsx
+++ src/app/blogs/[slug]/page.tsx
@@ -1,8 +1,23 @@
-export default function Page() {
+import { getPostsBySlug } from "@/app/lib/posts";
+import { notFound } from "next/navigation";
+
+type blogPostParams = {
+  params: {
+    slug: string;
+  };
+};
+
+export default function Page({ params }: blogPostParams) {
+  const post = getPostsBySlug(params.slug);
+
+  if (!post) {
+    notFound();
+  }
+
   return (
     <div>
-      <h1>This is post title</h1>
-      <p>This is post content</p>
+      <h1>{post.title}</h1>
+      <p>{post.content}</p>
     </div>
   );
 }
