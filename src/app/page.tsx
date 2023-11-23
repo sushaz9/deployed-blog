47fe2f79e9fe8257cbcf7f6c0d94f4495bd1fac7
sushaz9 <susannahelisabeth1@gmail.com>
2023-11-23 09:37:08 +0000

    test change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 import Image from "next/image";
 
-export default function Page() {
+export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>This is blogs page</h1>
+      <h1>This is main page</h1>
     </main>
   );
 }
